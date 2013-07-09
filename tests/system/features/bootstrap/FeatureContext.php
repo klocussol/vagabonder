ec5ada098a58f2b477507dff11a71e99f3c1e235
Jason Fox <jasonrobertfox@gmail.com>
2013-07-09 19:14:30 -0400

    installed the default behat setup

---
+++ tests/system/features/bootstrap/FeatureContext.php
@@ -0,0 +1,25 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface;
+use Behat\Behat\Context\TranslatedContextInterface;
+use Behat\Behat\Context\BehatContext;
+use Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+}
