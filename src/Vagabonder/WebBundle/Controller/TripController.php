bf49412180aa3d4746c0c8f4e0f6aa8a2f826371
Kyoko <kyoko.locussol@gmail.com>
2013-07-29 19:10:32 -0400

    Removed confirmation view, routing, and action

--- src/Vagabonder/WebBundle/Controller/TripController.php
+++ src/Vagabonder/WebBundle/Controller/TripController.php
@@ -10,10 +10,4 @@ class TripController extends Controller
     {
         return $this->render('VagabonderWebBundle:Trip:new.html.twig');
     }
-
-    public function confirmationAction()
-    {
-        return $this->render('VagabonderWebBundle:Trip:confirmation.html.twig');
-    }
-
 }
