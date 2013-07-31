6798b87b3052f8003b2d29812b99af45807326d1
Kyoko <kyoko.locussol@gmail.com>
2013-07-30 20:56:23 -0400

    Queried database in destination controller to get search term matches

---
+++ src/Vagabonder/WebBundle/Controller/DestinationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Vagabonder\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Vagabonder\WebBundle\Entity\Destination;
+
+class DestinationController extends Controller
+{
+    public function searchAction()
+    {
+        $term = $this->getRequest()->get('term');
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT d FROM VagabonderWebBundle:Destination d WHERE d.city LIKE \'' . $term. '%\'');
+
+        $destinations = $query->getResult();
+
+        $results = [];
+
+        foreach ($destinations as $destination) {
+            $results[$destination->getId()] = $destination->getCity();
+        }
+        $response = new Response(json_encode($results));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
